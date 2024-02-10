68852c765a69b7246c1d6e6c178350ed3bbc5372
moomoo3050 <tosabi@naver.com>
2024-02-10 20:14:33 +0900

    modified:   index.html 	modified:   main.js

---
+++ main.js
@@ -0,0 +1,14 @@
+async function getDNS(domain, type, service){
+    if (type === "A") {
+        await fetch(`https://1.1.1.1/dns-query?name=${domain}&type=${type}`, {headers: {"accept": "application/dns-json"}})
+        .then(response=>response.json())
+        .then(data=>{ return (data.Answer[0].data); }); // return ip
+    } else if (type === "SRV") {
+        // mcv.kr은 TCP연결을 지원하지 않기에 추후 수정 예정
+        fetch(`https://1.1.1.1/dns-query?name=_${service}._tcp.moomoo3050.mcv.kr&type=SRV`, {headers: {"accept": "application/dns-json"}})
+        .then(response=>response.json())
+        .then(data=>{ console.log(data.Answer[0].data.split(' ').at(-1)); })
+    } else {
+        alert("오류")
+    }
+}
