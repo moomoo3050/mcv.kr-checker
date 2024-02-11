b5a4890475f0cd603d944f21d5111fd6f3a15413
moomoo3050 <tosabi@naver.com>
2024-02-11 15:29:51 +0900

    modified:   README.md 	modified:   main.js

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 async function getDNS(domain, type, service){
     if (type === "A") {
-        await fetch(`https://1.1.1.1/dns-query?name=${domain}&type=${type}`, {headers: {"accept": "application/dns-json"}})
+        await fetch(`https://1.1.1.1/dns-query?name=${domain}.mcv.kr&type=${type}`, {headers: {"accept": "application/dns-json"}})
         .then(response=>response.json())
         .then(data=>{ return (data.Answer[0].data); }); // return ip
     } else if (type === "SRV") {
         // mcv.kr은 TCP연결을 지원하지 않기에 추후 수정 예정
         await fetch(`https://1.1.1.1/dns-query?name=_${service}._tcp.${domain}.mcv.kr&type=SRV`, {headers: {"accept": "application/dns-json"}})
         .then(response=>response.json())
-        .then(data=>{ return data.Answer[0].data.split(' ').at(-1); });
+        .then(data=>{ return data.Answer[0].data.split(' ').at(-1); }); // 
     } else {
         alert("오류");
     }
@@ -21,17 +21,24 @@ async function getIPaddr(){
     }); 
 }
 
-async function getMCVKRInfo(addr){
-    // check mcv.kr
-
-
-    await fetch(`https://1.1.1.1/dns-query?name=_${service}._tcp.${domain}.mcv.kr&type=SRV`, {headers: {"accept": "application/dns-json"}})
-        .then(response=>response.json())
-        .then(data=>{ return data.Answer[0].data.split(' ').at(-2); });
+async function getMCVKRInfo(domain, service){
+            await fetch(`https://1.1.1.1/dns-query?name=${await getDNS(domain, "SRV", service) /* 보안 주소 리턴 */}&type=A`, {headers: {"accept": "application/dns-json"}}) //보안 주소 질의
+            .then(response=>response.json())
+            .then(data=>{ return (data.Answer[0].data); });
+                if (data.Answer[0].data === "pipefiler.mcv.kr"){ // pipefiler.mcv.kr(보안주소)라면 서버 고유번호 리턴
+                    await fetch(`https://1.1.1.1/dns-query?name=_${service}._tcp.${domain}.mcv.kr&type=SRV`, {headers: {"accept": "application/dns-json"}})
+                    .then(response=>response.json())
+                    .then(data=>{
+                        return data.Answer[0].data.split(' ').at(-2);
+                    });
+                } else {
+                    return -1;
+                }
+        });
 }
 
 function check(){
     var sv-addr = document.querySelecter("#sv-addr").value;
     var ip-addr = document.querySelecter("#ip").value;
-
+    
 }
