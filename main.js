95705f8d99b76576bf7cace10acd351b76cba7f6
moomoo3050 <tosabi@naver.com>
2024-02-11 21:17:07 +0900

    modified:   README.md 	modified:   index.html 	modified:   main.css 	modified:   main.js

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
-async function getDNS(domain, type, service){
+var server_id = undefined
+
+function getDNS(domain, type, service){
     if (type === "A") {
-        await fetch(`https://1.1.1.1/dns-query?name=${domain}.mcv.kr&type=${type}`, {headers: {"accept": "application/dns-json"}})
+        fetch(`https://1.1.1.1/dns-query?name=${domain}.mcv.kr&type=${type}`, {headers: {"accept": "application/dns-json"}})
         .then(response=>response.json())
-        .then(data=>{ return (data.Answer[0].data); }); // return ip
+        .then(resp=>{
+            return resp.Answer[0].data;
+        }); // return ip
     } else if (type === "SRV") {
         // mcv.kr은 TCP연결을 지원하지 않기에 추후 수정 예정
-        await fetch(`https://1.1.1.1/dns-query?name=_${service}._tcp.${domain}.mcv.kr&type=SRV`, {headers: {"accept": "application/dns-json"}})
+        fetch(`https://1.1.1.1/dns-query?name=_${service}._tcp.${domain}.mcv.kr&type=SRV`, {headers: {"accept": "application/dns-json"}})
         .then(response=>response.json())
-        .then(data=>{ return data.Answer[0].data.split(' ').at(-1); }); // 
+        .then(resp=>{
+            return resp.Answer[0].data.split(' ').at(-1);
+        }); // 
     } else {
         alert("오류");
     }
@@ -21,17 +27,17 @@ async function getIPaddr(){
     }); 
 }
 
-async function getMCVKRInfo(domain, service, ip_addr){
-            await fetch(`https://1.1.1.1/dns-query?name=${await getDNS(domain, "SRV", service) /* 보안 주소(추정) 리턴 */}&type=A`, {headers: {"accept": "application/dns-json"}}) //보안 주소 질의
+function getMCVKRInfo(domain, service, ip_addr){
+            fetch(`https://1.1.1.1/dns-query?name=${getDNS(domain, "SRV", service).split(' ').at(-1) /* 보안 주소(추정) 리턴 */}&type=A`, {headers: {"accept": "application/dns-json"}}) //보안 주소 질의
             .then(response=>response.json())
-            .then(data=>{  
-                if (data.Answer[0].data === "pipefiler.mcv.kr"){ // pipefiler.mcv.kr(보안주소)라면 서버 고유번호 리턴
+            .then(resp=>{  
+                if (resp.Answer[0].data === "pipefiler.mcv.kr"){ // pipefiler.mcv.kr(보안주소)라면 서버 고유번호 리턴
                     fetch(`https://1.1.1.1/dns-query?name=_${service}._tcp.${domain}.mcv.kr&type=SRV`, {headers: {"accept": "application/dns-json"}})
                     .then(response=>response.json())
-                    .then(data=>{
-                        return data.Answer[0].data.split(' ').at(-2);
+                    .then(resp2=>{
+                        return resp2.Answer[0].data.split(' ').at(-2);
                     });
-                } else if (data.Answer[0].data === ip_addr) { //보안 주소 아니라면 ip 주소 여부
+                } else if (resp.Answer[0].data === ip_addr) { //보안 주소 아니라면 ip 주소 여부
                     return -1;
                 } else { // 아무것도 아닐 때
                     return false
@@ -41,25 +47,52 @@ async function getMCVKRInfo(domain, service, ip_addr){
 
 function check(){
     
-    var result = {"A":"","SRV":"","SRV_A":""}
-
+    var result = {"A":"","SRV":"","SRV_A":""} // true: 위험
+ 
     // input 수집
     var sv_addr = document.querySelector("#sv-addr").value; // 서버 주소(mcv.kr 제외)
     var ip_addr = document.querySelector("#ip").value; //ip 주소
-    var service = document.querySelector("#service").options[e.selectedIndex].text;
+    var service = document.querySelector("#service").options[document.querySelector("#service").selectedIndex].text;
 
     // 설명 수집
-    var reshtml = document.querySelector("#result");
-    var desc = document.querySelector("#desc");
+    
+    let A = document.querySelector("#A")
+    let SRV = document.querySelector("#SRV")
+    let MCV = document.querySelector("#MCV")
 
     //동일 확인
     if (getDNS(sv_addr, "A") === ip_addr) { //서버주소 A레코드
-        result
-    } else if () { // 서버주소 SRV레코드
-        
-    } else if () {// SRV레코드에 연결된 A레코드
+        result["A"] = true
+    }
+    if (getDNS(sv_addr, "SRV", service) === ip_addr) {
+        result["SRV"] = true
+    }
+    mcv_data = getMCVKRInfo(sv_addr, service, ip_addr)
+    if (mcv_data === false) { // ip 주소도 아니면서 보안주소도 아닐때
+        result["SRV_A"] = -1 // -1: 확인되지 않음(ip주소가 일치하지 않으나 mcv.kr에 연결되지 않음)
+    } else if (mcv_data === -1) { // ip일때
+        result["SRV_A"] = true
+    } else { // mcv.kr 서버 고유번호 리턴
+        result["SRV_A"] = mcv_data
+    }
 
+    //설명 쓰기
+    if (result["A"]) {
+        A.innerHTML = "위험"
     } else {
-        
+        A.innerHTML = "안전"
+    }
+    if (result["SRV"]) {
+        SRV.innerHTML = "위험" 
+    } else {
+        SRV.innerHTML = "안전" 
+    }
+    if (result["SRV_A"]) {
+        MCV.innerHTML = "위험"
+    }
+    if (result["SRV_A"] === -1) {
+        MCV.innerHTML = "mcv.kr에 연결되지 않음"
+    } else {
+        MCV.innerHTML = `안전(고유번호: ${result["SRV_A"]}`
     }
 }
