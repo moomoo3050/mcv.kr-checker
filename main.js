e55b0c169df152247f98e3a71ebf627d83f61969
moomoo3050 <tosabi@naver.com>
2024-02-11 18:43:43 +0900

    modified:   README.md 	modified:   index.html 	modified:   main.js

--- main.js
+++ main.js
@@ -17,28 +17,49 @@ async function getIPaddr(){
     await fetch('https://api.ipify.org?format=json')
     .then(response=>response.json())
     .then(data=>{
-        document.querySelecter("#ip").value = data.ip;
+        document.querySelector("#ip").value = data.ip;
     }); 
 }
 
-async function getMCVKRInfo(domain, service){
-            await fetch(`https://1.1.1.1/dns-query?name=${await getDNS(domain, "SRV", service) /* 보안 주소 리턴 */}&type=A`, {headers: {"accept": "application/dns-json"}}) //보안 주소 질의
+async function getMCVKRInfo(domain, service, ip_addr){
+            await fetch(`https://1.1.1.1/dns-query?name=${await getDNS(domain, "SRV", service) /* 보안 주소(추정) 리턴 */}&type=A`, {headers: {"accept": "application/dns-json"}}) //보안 주소 질의
             .then(response=>response.json())
-            .then(data=>{ return (data.Answer[0].data); });
+            .then(data=>{  
                 if (data.Answer[0].data === "pipefiler.mcv.kr"){ // pipefiler.mcv.kr(보안주소)라면 서버 고유번호 리턴
-                    await fetch(`https://1.1.1.1/dns-query?name=_${service}._tcp.${domain}.mcv.kr&type=SRV`, {headers: {"accept": "application/dns-json"}})
+                    fetch(`https://1.1.1.1/dns-query?name=_${service}._tcp.${domain}.mcv.kr&type=SRV`, {headers: {"accept": "application/dns-json"}})
                     .then(response=>response.json())
                     .then(data=>{
                         return data.Answer[0].data.split(' ').at(-2);
                     });
-                } else {
+                } else if (data.Answer[0].data === ip_addr) { //보안 주소 아니라면 ip 주소 여부
                     return -1;
+                } else { // 아무것도 아닐 때
+                    return false
                 }
-        });
+            });
 }
 
 function check(){
-    var sv-addr = document.querySelecter("#sv-addr").value;
-    var ip-addr = document.querySelecter("#ip").value;
     
+    var result = {"A":"","SRV":"","SRV_A":""}
+
+    // input 수집
+    var sv_addr = document.querySelector("#sv-addr").value; // 서버 주소(mcv.kr 제외)
+    var ip_addr = document.querySelector("#ip").value; //ip 주소
+    var service = document.querySelector("#service").options[e.selectedIndex].text;
+
+    // 설명 수집
+    var reshtml = document.querySelector("#result");
+    var desc = document.querySelector("#desc");
+
+    //동일 확인
+    if (getDNS(sv_addr, "A") === ip_addr) { //서버주소 A레코드
+        result
+    } else if () { // 서버주소 SRV레코드
+        
+    } else if () {// SRV레코드에 연결된 A레코드
+
+    } else {
+        
+    }
 }
